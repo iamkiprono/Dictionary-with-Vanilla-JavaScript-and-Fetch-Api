f41c842c806fb06151cf93bf8e0652475998a40c
iamkiprono <101986591+iamkiprono@users.noreply.github.com>
2023-01-28 08:29:05 +0300

    Update app.js
    The audio function was not working so had to use a for loop to continue the loop a when the audio file came back empty
    
    Then create a variable if the audio file was existent

--- app.js
+++ app.js
@@ -1,41 +1,39 @@
 const searchBtn = document.querySelector(".search");
 
 searchBtn.addEventListener("click", () => {
+  const searchValue = document.querySelector(".search-box").value;
 
-    const searchValue = document.querySelector(".search-box").value;
-    console.log(searchValue)
-
-
-    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchValue}`)
-      .then((respone) => respone.json())
-      .then((data) => {
-        const name = data[0].word;
-        const phonetic = data[0].phonetic;
-        const partOfSpeech = data[0].meanings[0].partOfSpeech;
-        const meaning = data[0].meanings[0].definitions[0].definition;
-        const audioFile = data[0].phonetics[1].audio;
-        console.log(audioFile)
+  fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchValue}`)
+  .then((respone) => respone.json())
+  .then((data) => {
+      const name = data[0].word;
+      const phonetic = data[0].phonetic;
+      const partOfSpeech = data[0].meanings[0].partOfSpeech;
+      const meaning = data[0].meanings[0].definitions[0].definition;
+      
+      const updateName = document.querySelector(".name");
+      const updatePhonetic = document.querySelector(".phonetic");
+      const updateSpeech = document.querySelector(".partofspeech");
+      const updateMeaning = document.querySelector(".meaning");
     
-        const updateName = document.querySelector(".name");
-        const updatePhonetic = document.querySelector(".phonetic");
-        const updateSpeech = document.querySelector(".partofspeech");
-        const updateMeaning = document.querySelector(".meaning");
-    
-        updateName.textContent = name
-        updatePhonetic.textContent = phonetic
-        updateSpeech.textContent = partOfSpeech
-        updateMeaning.textContent = meaning
-        
-        const playBtn = document.querySelector('.play')
-
+      updateName.textContent = name;
+      updatePhonetic.textContent = phonetic;
+      updateSpeech.textContent = partOfSpeech;
+      updateMeaning.textContent = meaning;
+      for (let i = 0; i < 2; i++) {
+        if (data[0].phonetics[i].audio == "") {
+          continue;
+        } else {
+        }
+        const audioFile = data[0].phonetics[i].audio;
+        console.log(audioFile);
+        const playBtn = document.querySelector(".play");
         playBtn.addEventListener("click", () => {
-            const mediaFile = document.querySelector('.media')
-            mediaFile.src = audioFile
-            mediaFile.play()
-        })
-      });
-})
+          const mediaFile = document.querySelector(".media");
+          mediaFile.src = audioFile;
+          mediaFile.play();
+        });
+      }
 
-
-
-
+    });
+});
